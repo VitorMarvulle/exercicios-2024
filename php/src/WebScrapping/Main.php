b0cf09d48b7b749b709d1e069ae97efb5354c7c5
VitorMarvulle <marvullevitor@gmail.com>
2024-05-04 14:27:53 -0700

    feat: insercao Spout e testes para .xlxs

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,8 @@
 
 namespace Chuva\Php\WebScrapping;
 libxml_use_internal_errors(true); //nao imprime os warnings de tags (CSS) inválidas
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
 
 /**
  * Runner for the Webscrapping exercice.
@@ -16,15 +18,29 @@ class Main {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
     $xPath = new \DOMXPath($dom);
+    $filePath = './assets/model.xlsx';
    
     $scrapper = new Scrapper();
     $papers = $scrapper->scrap($xPath);
 
-    foreach ($papers as $paper){
-      $paper->id;
-    }
+    print_r($papers);  //recupera todos os dados e armazena em arrays separadamente
 
-    print_r($papers);
+  
+
+
+  $writer = WriterEntityFactory::createXLSXWriter();
+
+  
+  $writer->openToFile($filePath); // write data to a file or to a PHP stream
+  //$writer->openToBrowser($fileName); // stream data directly to the browser
+
+  
+  /** Shortcut: add a row from an array of values */
+  $values = ['Carl', 'is', 'greataaasdasdasdas!'];
+  $rowFromValues = WriterEntityFactory::createRowFromArray($values);
+  $writer->addRow($rowFromValues);
+  
+  $writer->close();
 
   }
 
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -18,25 +18,19 @@ class Scrapper {
     $NodeListId = $xPath->query("//div[@class='volume-info']/text()");
     $NodeListTitle = $xPath->query("//h4[@class='my-xs paper-title']/text()");
     $NodeListType = $xPath->query("//div[@class='tags mr-sm']/text()");
-    
-    
-    $papers = []; //armazenar os dados dos papers em array
 
-    for ($i = 1; $i < $NodeList->length + 1; $i++) { //usarei como índice para iterar, o xPath dos cards
-      $NodeListAuthors = $xPath->query("//a[$i]//div[@class='authors']/span/text()");
-      $NodeListAuthors_name = $xPath->query("//a[$i]//div[@class='authors']/span/text()");
-      $NodeListAuthors_inst = $xPath->query("//a[$i]//div[@class='authors']//span/@title");
+    //iteração id, title, type
+    for ($i = 0; $i < $NodeList->length; $i++) {      //usarei o xPath dos cards como índice para iterar
       $id = $NodeListId->item($i)->textContent;
       $title = $NodeListTitle->item($i)->textContent;
       $type = $NodeListType->item($i)->textContent;
       
-      ///imprimindo tamanho da nodeList para testar
-      echo $NodeListAuthors->length;
-      echo "\n";
       $authors = [];
 
-      
-        for ($j = 0; $j < $NodeListAuthors->length; $j++){
+    //iteração authors_name, authors_inst  
+      $NodeListAuthors = $xPath->query("//a[$i+1]//div[@class='authors']/span/text()");        $NodeListAuthors_name = $xPath->query("//a[$i+1]//div[@class='authors']/span/text()");
+      $NodeListAuthors_inst = $xPath->query("//a[$i+1]//div[@class='authors']//span/@title");
+       for ($j = 0; $j < $NodeListAuthors->length; $j++){
             
             $name = $NodeListAuthors_name->item($j)->textContent;
             $institution = $NodeListAuthors_inst->item($j)->textContent;
