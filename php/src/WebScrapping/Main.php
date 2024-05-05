6b7b59b82fd10b6fe9ed78cb661877c70eb8d265
VitorMarvulle <marvullevitor@gmail.com>
2024-05-04 18:42:58 -0700

    resolução final

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,47 +1,80 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
+
 libxml_use_internal_errors(true); //nao imprime os warnings de tags (CSS) inválidas
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
-use Box\Spout\Common\Entity\Row;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Common\Entity\Style\Color;
 
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main {
+class Main
+{
 
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
-    
+  public static function run(): void
+  {
+
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
     $xPath = new \DOMXPath($dom);
     $filePath = './assets/model.xlsx';
-   
+
     $scrapper = new Scrapper();
     $papers = $scrapper->scrap($xPath);
 
     print_r($papers);  //recupera todos os dados e armazena em arrays separadamente
 
-  
+    //*//
+    //SCRIPT para escrever no doc model.xlsx usando a lib Spout.
+    $writer = WriterEntityFactory::createXLSXWriter();
+    $writer->openToFile($filePath);
 
+    $maxAuthors = 0;  //busca paper com maior número de autores para gerar o Header da planilha.
+    foreach ($papers as $paper) {
+      $numAuthors = count($paper->authors);
+      if ($numAuthors > $maxAuthors) {
+        $maxAuthors = $numAuthors;
+      }
+    }
 
-  $writer = WriterEntityFactory::createXLSXWriter();
+    // Headers (Author/Author Inst) dinâmicos com base no número máximo de autores coletados no scrapper.
+    $headers = ['ID', 'Title', 'Type'];
+    for ($i = 1; $i < $maxAuthors-1; $i++) {
+      $headers[] = "Author{$i}";
+      $headers[] = "Author{$i} Institution";
+    }
 
-  
-  $writer->openToFile($filePath); // write data to a file or to a PHP stream
-  //$writer->openToBrowser($fileName); // stream data directly to the browser
+    //estilização simples da planilha com fontBold e bg-color dos headers.
+    $rowStyle = (new StyleBuilder())
+    ->setFontBold()
+    ->setBackgroundColor(Color::rgb(118, 206, 250))
+    ->build();
+    $headerRow = WriterEntityFactory::createRowFromArray($headers, $rowStyle);
+    $writer->addRow($headerRow);
 
-  
-  /** Shortcut: add a row from an array of values */
-  $values = ['Carl', 'is', 'greataaasdasdasdas!'];
-  $rowFromValues = WriterEntityFactory::createRowFromArray($values);
-  $writer->addRow($rowFromValues);
-  
-  $writer->close();
+    foreach ($papers as $paper) {
+      $id = $paper->id;
+      $title = $paper->title;
+      $type = $paper->type;
+      $authors = $paper->authors; // array de objetos Person
+      $rowPaper = [$id, $title, $type];
 
+      foreach ($authors as $author) {
+        $rowPaper[] = $author->name;
+        $rowPaper[] = $author->institution;
+      }
+
+      // linha com os dados do paper já separados por colunas.
+      $row = WriterEntityFactory::createRowFromArray($rowPaper);
+      $writer->addRow($row);
+    }
+
+    $writer->close();
   }
 
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -8,12 +8,14 @@ use Chuva\Php\WebScrapping\Entity\Person;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
 
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
-  public function scrap(\DOMXPath $xPath): array {
+  public function scrap(\DOMXPath $xPath): array
+  {
     $NodeList = $xPath->query("//a[@class='paper-card p-lg bd-gradient-left']");
     $NodeListId = $xPath->query("//div[@class='volume-info']/text()");
     $NodeListTitle = $xPath->query("//h4[@class='my-xs paper-title']/text()");
@@ -24,32 +26,31 @@ class Scrapper {
       $id = $NodeListId->item($i)->textContent;
       $title = $NodeListTitle->item($i)->textContent;
       $type = $NodeListType->item($i)->textContent;
-      
+
       $authors = [];
 
-    //iteração authors_name, authors_inst  
-      $NodeListAuthors = $xPath->query("//a[$i+1]//div[@class='authors']/span/text()");        $NodeListAuthors_name = $xPath->query("//a[$i+1]//div[@class='authors']/span/text()");
+      //iteração authors_name, authors_inst  
+      $NodeListAuthors = $xPath->query("//a[$i+1]//div[@class='authors']/span/text()");
+      $NodeListAuthors_name = $xPath->query("//a[$i+1]//div[@class='authors']/span/text()");
       $NodeListAuthors_inst = $xPath->query("//a[$i+1]//div[@class='authors']//span/@title");
-       for ($j = 0; $j < $NodeListAuthors->length; $j++){
-            
-            $name = $NodeListAuthors_name->item($j)->textContent;
-            $institution = $NodeListAuthors_inst->item($j)->textContent;
-            $authors[] = new Person(
-              $name,
-              $institution
-            );
-          }
-      
+      for ($j = 0; $j < $NodeListAuthors->length; $j++) {
+
+        $name = $NodeListAuthors_name->item($j)->textContent;
+        $institution = $NodeListAuthors_inst->item($j)->textContent;
+        $authors[] = new Person(
+          $name,
+          $institution
+        );
+      }
+
       $papers[] = new Paper(
-          $id,
-          $title,
-          $type,
-          $authors
+        $id,
+        $title,
+        $type,
+        $authors
       );
+    }
+    return $papers;
   }
-  return $papers;
-}
-
-
 
 }
